85e4df66801107dd24443935bea6835f522dc7e3
KorotkayaNastuha <skiteikina11@gmail.com>
2023-02-07 01:25:57 +0200

    kk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import { RestrictedRoute } from "./RestrictedRoute";
 import { PrivateRoute } from "./PrivateRoute";
 
 
-const HomePage = lazy(() => import('../pages/Home'));
+const HomePage = lazy(() => import('../pages/home'));
 const RegisterPage = lazy(() => import('../pages/Register'));
 const LoginPage = lazy(() => import('../pages/Login'));
 const ContactsPage = lazy(() => import('../pages/Contacts'));
