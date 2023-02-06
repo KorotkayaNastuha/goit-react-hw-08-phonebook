bbe344b21c4c03af9a4316c94c6988e3f0904107
KorotkayaNastuha <skiteikina11@gmail.com>
2023-02-07 00:17:25 +0200

    fff

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,16 +1,16 @@
-// import { useAuth } from "hooks/useAuth";
-// import { Navigation } from "components/Navigation/Navigation";
-// import { UserMenu } from "components/UserMenu/UserMenu";
-// import { AuthNav } from "components/AuthNav/AuthNav";
+import { useAuth } from "hooks/useAuth";
+import { Navigation } from "components/Navigation/Navigation";
+import { UserMenu } from "components/UserMenu/UserMenu";
+import { AuthNav } from "components/AuthNav/AuthNav";
 
-// export const AppBar = () => {
-//     const { isLoggedIn } = useAuth();
-//     return (
-//         <header>
-//             <Navigation />
-//             {isLoggedIn ? <UserMenu /> : <AuthNav />
+export const AppBar = () => {
+    const { isLoggedIn } = useAuth();
+    return (
+        <header>
+            <Navigation />
+            {isLoggedIn ? <UserMenu /> : <AuthNav />
             
-//             }
-//         </header>
-//     )
-// }
+            }
+        </header>
+    )
+}
