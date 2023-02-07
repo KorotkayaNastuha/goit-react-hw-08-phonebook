842d068e6814e516d2df1939c14754d4fd9306b2
KorotkayaNastuha <skiteikina11@gmail.com>
2023-02-08 01:15:14 +0200

    refactoring

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { logIn } from 'redux/auth/operations';
 
-
+import { Box, Button, TextField } from '@mui/material';
 export const LoginForm = () => {
     const dispatch = useDispatch();
 
@@ -17,17 +17,58 @@ export const LoginForm = () => {
         form.reset();
     };
     return (
-        <form onSubmit={handleSubmit} autoComplete="off">
-            <label>
-                Email
-                <input type="email" name="email"/>
-            </label>
-            <label>
-                Password
-                <input type="password" name="password"/>
-            </label>
-            <button type="submit">Log In</button>
-            </form>
+
+        <Box sx={{ width: '280px', mt: 4}}>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          fullWidth
+          required
+          sx={{ mb: '1rem' }}
+          variant="outlined"
+          id="email"
+          name="email"
+          type="email"
+          label="Email"
+          value={dispatch.email}
+        //   onChange={formik.handleChange}
+        //   error={formik.touched.email && Boolean(formik.errors.email)}
+        //   helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          required
+          sx={{ mb: '1rem' }}
+          variant="outlined"
+          id="password"
+          name="password"
+          type="password"
+          label="Password"
+          value={dispatch.password}
+        //   onChange={formik.handleChange}
+        //   error={formik.touched.password && Boolean(formik.errors.password)}
+        //   helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button
+          variant="contained"
+          type="submit"
+          size="large"
+          sx={{ width: '50%', marginLeft: '25%' }}
+        >
+          Log In
+        </Button>
+      </form>
+    </Box>
+        // <form onSubmit={handleSubmit} autoComplete="off">
+        //     <label>
+        //         Email
+        //         <input type="email" name="email"/>
+        //     </label>
+        //     <label>
+        //         Password
+        //         <input type="password" name="password"/>
+        //     </label>
+        //     <button type="submit">Log In</button>
+        //     </form>
        
     )
 }
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,6 +1,8 @@
 import { useDispatch } from "react-redux";
 import { register } from "redux/auth/operations";
 
+import { Box, Button, TextField } from '@mui/material';
+
 export const RegisterForm = () => {
     const dispatch = useDispatch();
 
@@ -17,21 +19,77 @@ export const RegisterForm = () => {
         form.reset();
     };
     return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-        <label>
-            UserName
-            <input type="text" name="name"/>
-        </label>
-        <label>
-            Email
-            <input type="email" name="email"/>
-        </label>
-        <label>
-            Password
-                <input type="password" name="password" />
-        </label>        
-            <button type="submit">Register</button>
+        <Box sx={{ width: '280px', mt: 4}}>
+            <form autoComplete="off" onSubmit={handleSubmit}
+            
+            >
+        <TextField
+          fullWidth
+          required
+          sx={{ mb: '1rem' }}
+          variant="outlined"
+          id="name"
+          name="name"
+          type="text"
+          label="Name"
+        //   value={dispatch.name}
+        //   onChange={handleChange}
+        //   error={touched.name && Boolean(errors.name)}
+        //   helperText={touched.name && formik.errors.name}
+        />
+        <TextField
+          fullWidth
+          required
+          sx={{ mb: '1rem' }}
+          variant="outlined"
+          id="email"
+          name="email"
+          type="email"
+          label="Email"
+          value={dispatch.email}
+        //   onChange={handleChange}
+        //   error={formik.touched.email && Boolean(formik.errors.email)}
+        //   helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          required
+          sx={{ mb: '1rem' }}
+          variant="outlined"
+          id="password"
+          name="password"
+          type="password"
+          label="Password"
+          value={dispatch.password}
+        //   onChange={handleChange}
+        //   error={formik.touched.password && Boolean(formik.errors.password)}
+        //   helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button
+          variant="contained"
+          type="submit"
+          size="small"
+          sx={{ width: '50%' , marginLeft: '25%'}}
+        >
+          Register
+        </Button>
+      </form>
+    </Box>
+    // <form onSubmit={handleSubmit} autoComplete="off">
+    //     <label>
+    //         UserName
+    //         <input type="text" name="name"/>
+    //     </label>
+    //     <label>
+    //         Email
+    //         <input type="email" name="email"/>
+    //     </label>
+    //     <label>
+    //         Password
+    //             <input type="password" name="password" />
+    //     </label>        
+    //         <button type="submit">Register</button>
         
-        </form> 
+    //     </form> 
     )
 }
